789744e3505a6ae1de34cabffb73d7698de14903
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-01 22:46:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,16 @@
 def square_array(numbers)
+<<<<<<< HEAD
   result = Array.new(numbers.size)
+=======
+>>>>>>> 489c4d44c145052646bc5e26a9781e2c8d76b9aa
   i = 0
   while i < numbers.size
     result[i] = numbers[i] ** 2
     i += 1
   end
   result
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 489c4d44c145052646bc5e26a9781e2c8d76b9aa
